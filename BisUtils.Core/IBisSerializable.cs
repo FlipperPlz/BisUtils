236324b92b47d25c7197fe81f515e178c40852b1
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2022-11-30 00:20:37 -0500

    feat(core): Add IBisSerializable With Common Options

--- BisUtils.Core/IBisSerializable.cs
+++ BisUtils.Core/IBisSerializable.cs
@@ -7,6 +7,27 @@ public abstract class BisDeserializationOptions : BisOptions {
 public abstract class BisSerializationOptions : BisOptions {
 }
 
+public abstract class BisCommonSerializationOptions : BisOptions {
+}
+
+public interface IBisSerializable<SO> where SO : BisCommonSerializationOptions {
+    public IBisSerializable ReadBinary(BinaryReader reader, SO deserializationOptions);
+    public void WriteBinary(BinaryWriter writer, SO serializationOptions);
+    
+    public virtual byte[] Binarize(SO serializationOptions) {
+        var stream = new MemoryStream();
+        using var writer = new BinaryWriter(stream);
+        WriteBinary(writer, serializationOptions);
+        return stream.ToArray();
+    }
+
+    public virtual IBisSerializable Debinarize(byte[] data, SO deserializationOptions) => 
+        ReadBinary(new BinaryReader(new MemoryStream(data)), deserializationOptions);
+
+    public static T FromBinary<T>(byte[] data, SO deserializationOptions) where T : IBisSerializable<SO>, new() => (T) new T().Debinarize(data, deserializationOptions);
+}
+
+
 public interface IBisSerializable<DSO, SO> where DSO : BisDeserializationOptions where SO : BisSerializationOptions {
     public IBisSerializable ReadBinary(BinaryReader reader, DSO deserializationOptions);
     public void WriteBinary(BinaryWriter writer, SO serializationOptions);
