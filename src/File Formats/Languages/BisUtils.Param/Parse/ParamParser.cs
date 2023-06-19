2e5da4d1ab9d1676cfb75b7438e5ef921203112a
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-06-18 18:29:23 -0700

    feat: Reformat Project In Preparation For The RV PreProcessor

---
+++ src/File Formats/Languages/BisUtils.Param/Parse/ParamParser.cs
@@ -0,0 +1,15 @@
+﻿namespace BisUtils.Param.Parse;
+
+using BisUtils.Param.Models.Stubs;
+using Models;
+
+public static class ParamLiteralParser
+{
+
+    public static ParamFile Parse(string contents, string filename)
+    {
+        var paramFile = new ParamFile(filename, new List<IParamStatement>());
+
+        throw new NotImplementedException();
+    }
+}
---
+++ src/File Formats/Languages/BisUtils.Param/Parse/ParamStringStepper.cs
@@ -0,0 +1,12 @@
+﻿namespace BisUtils.Param.Parse;
+
+using Core.Parsing;
+using FResults;
+
+public class ParamStringStepper : BisMutableStringStepper
+{
+    public ParamStringStepper(string content) : base(content)
+    {
+    }
+
+}
