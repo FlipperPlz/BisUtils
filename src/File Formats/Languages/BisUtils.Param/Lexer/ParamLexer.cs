a66f1a00fd19057679ef0db8104337042c0491a9
Ryann (Flipper) <98256177+FlipperPlz@users.noreply.github.com>
2023-06-20 07:12:08 -0700

    feat(param): Begin Writing Param Parser

---
+++ src/File Formats/Languages/BisUtils.Param/Lexer/ParamLexer.cs
@@ -0,0 +1,103 @@
+﻿namespace BisUtils.Param.Lexer;
+
+using System.Text;
+using Core.Parsing.Errors;
+using FResults;
+using PreProcessor.RV.Lexer;
+
+public class ParamLexer : RVLexer
+{
+    public ParamLexer(string content) : base(content)
+    {
+    }
+
+    public Result ReadIdentifier(out string keyword, bool allowEOF = false)
+    {
+        var results = new List<Result>() { TraverseWhitespace(out _) };
+        if(CurrentChar is null && !allowEOF)
+        {
+            keyword = "";
+            results.Add(Result.Fail(BisEndOfFileError.Instance));
+            return Result.Merge(results);
+        }
+
+        var builder = new StringBuilder();
+        while (CurrentChar is { } currentChar && (char.IsLetterOrDigit(currentChar) || currentChar == '_'))
+        {
+            builder.Append(currentChar);
+            MoveForward();
+        }
+
+        keyword = builder.ToString();
+        return Result.Merge(results);
+    }
+
+    public Result ReadString(out string stringRead, params char[] delimiters)
+    {
+        var results = new List<Result>() { TraverseWhitespace(out _) };
+        var quoted = false;
+        if (CurrentChar == '"')
+        {
+            quoted = true;
+            MoveForward();
+        }
+
+        var builder = new StringBuilder();
+
+        while (CurrentChar is { } currentChar && !delimiters.Contains(currentChar))
+        {
+            if (currentChar is '\n' or '\r')
+            {
+                if (!quoted)
+                {
+                    results.Add(TraverseWhitespace(out _));
+                    if (currentChar != '#')
+                    {
+                        // ReSharper disable once RedundantJumpStatement
+                        goto Finish;
+                    }
+                    //TODO PreProcessLine
+                    break;
+                }
+                results.Add(Result.Fail(BisEndOfLineError.Instance));
+                goto Finish;
+            }
+
+            if (currentChar == '"' && quoted)
+            {
+                if (MoveForward() != '"')
+                {
+                    results.Add(TraverseWhitespace(out _));
+                    if (currentChar != '\\')
+                    {
+                        goto Finish;
+                    }
+
+                    if (MoveForward() != 'n')
+                    {
+                        results.Add(Result.Fail("TODO")); //TODO);
+                        goto Finish;
+                    }
+                    results.Add(TraverseWhitespace(out _));
+                    if (CurrentChar != '"')
+                    {
+                        results.Add(Result.Fail("TODO")); //TODO);
+                        goto Finish;
+                    }
+
+                    builder.Append('\n');
+                    MoveForward();
+                }
+            }
+
+            builder.Append(CurrentChar);
+            MoveForward();
+        }
+        Finish:
+        {
+            stringRead = builder.ToString();
+            return Result.Merge(results);
+        }
+
+    }
+}
--- src/File Formats/Languages/BisUtils.Param/Parse/ParamParser.cs
+++ src/File Formats/Languages/BisUtils.Param/Parse/ParamParser.cs
@@ -1,15 +1,81 @@
 ﻿namespace BisUtils.Param.Parse;
 
 using BisUtils.Param.Models.Stubs;
+using Core.Parsing.Errors;
+using FResults;
+using Lexer;
 using Models;
+using PreProcessor.RV;
 
 public static class ParamLiteralParser
 {
 
-    public static ParamFile Parse(string contents, string filename)
+    public static Result Parse(string contents, string filename, IRVPreProcessor preProcessor, out ParamFile file)
     {
-        var paramFile = new ParamFile(filename, new List<IParamStatement>());
+        var results = new List<Result>();
+        file = new ParamFile(filename, new List<IParamStatement>());
+        var lexer = new ParamLexer(contents);
+        var stack = new Stack<IParamStatementHolder>();
+        stack.Push(file);
+        results.Add(preProcessor.ProcessLexer(lexer));
+        lexer.ResetLexer();
 
-        throw new NotImplementedException();
+        bool TryEnd()
+        {
+            if (stack is not { Count: 1 })
+            {
+                throw new IOException();
+            }
+
+            return true;
+        }
+
+        while (stack.Any())
+        {
+            var context = stack.Peek();
+            lexer.MoveForward();
+            results.Add(lexer.TraverseWhitespace(out _));
+
+            switch (lexer.CurrentChar)
+            {
+                case null:
+                {
+                    if (TryEnd())
+                    {
+                        goto Done;
+                    }
+
+                    break;
+                }
+                case '#': return Result.Fail(BisEndOfFileError.Instance); //TODO
+                case '}':
+                {
+                    lexer.MoveForward();
+                    lexer.TraverseWhitespace(out _);
+                    if(lexer.CurrentChar != ';')
+                    {
+                        return Result.Fail(BisEndOfFileError.Instance); //TODO
+                    }
+
+                    stack.Pop();
+                    continue;
+                }
+            }
+
+            results.Add(lexer.ReadIdentifier(out var keyword, true));
+
+            switch (keyword)
+            { //TODO: Parse Statements
+                default:
+                    break;
+            }
+
+        }
+        Done:
+        {
+
+            return Result.Merge(results);
+        }
     }
 }
+
