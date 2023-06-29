10717fd4829869560a0ca50ba1d654e0f35943ad
ryann <flipperplz@protonmail.com>
2023-06-29 14:32:48 -0700

    feat(core): Start Adding Support For PreProcessable Parser.

--- src/BisUtils.Core/Parsing/BisParser.cs
+++ src/BisUtils.Core/Parsing/BisParser.cs
@@ -7,20 +7,24 @@ using Lexer;
 #pragma warning disable CA1000
 public interface IBisParser<TAstNode, in TLexer, TTypes> where TLexer : BisLexer<TTypes> where TTypes : Enum
 {
-    public static abstract Result Parse(out TAstNode? node, TLexer lexer);
+    public static virtual Result Parse(out TAstNode? node, TLexer lexer) => throw new NotSupportedException();
 
 }
 
 public interface IBisParser<TAstNode, in TLexer, TTypes, in TPreprocessor> : IBisParser<TAstNode, TLexer, TTypes> where TLexer : BisLexer<TTypes> where TPreprocessor : BisPreProcessorBase, new() where TTypes : Enum
 {
-    // static Result IBisParser<TAstNode, TLexer, TTypes>.Parse(out TAstNode? node, TLexer lexer) => ;
-    //
-    // public static virtual Result ProcessAndParse(out TAstNode? node, TLexer lexer, TPreprocessor? preprocessor = null)
-    // {
-    //     preprocessor ??= new TPreprocessor();
-    //     var builder = new StringBuilder();
-    //     var preprocessResult = preprocessor.EvaluateLexer(lexer, builder);
-    //     lexer.ResetLexer(builder.ToString());
-    //     return Result.Merge(preprocessResult, Parse(out node, lexer));
-    // }
+    public static virtual Result ProcessAndParse
+    (
+        out TAstNode? node,
+        TLexer lexer,
+        TPreprocessor? preprocessor = null
+    )
+    {
+        var builder = new StringBuilder();
+        preprocessor ??= new TPreprocessor();
+        preprocessor.EvaluateLexer(lexer, builder);
+        lexer.ResetLexer(builder.ToString());
+        throw new NotSupportedException();
+        //return Parse(out node, lexer);
+    }
 }
