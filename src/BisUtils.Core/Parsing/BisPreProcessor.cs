7aaca1c9477625cd95bbd03308967bdc2dbc1ff3
ryann <flipperplz@protonmail.com>
2023-06-30 11:28:20 -0700

    docs: Add Bits Of Documentation To Parser Bases

--- src/BisUtils.Core/Parsing/BisPreProcessor.cs
+++ src/BisUtils.Core/Parsing/BisPreProcessor.cs
@@ -4,32 +4,66 @@ using System.Text;
 using FResults;
 using Lexer;
 
+/// <summary>
+/// Interface for the BIS PreProcessor objects.
+/// </summary>
 public interface IBisPreProcessorBase
 {
+    /// <summary>
+    /// Evaluate the lexer and if present, append outcome to the StringBuilder instance.
+    /// </summary>
+    /// <param name="lexer">IBisMutableStringStepper instance.</param>
+    /// <param name="builder">StringBuilder instance.</param>
+    /// <returns>Returns Result instance. </returns>
     public Result EvaluateLexer(IBisMutableStringStepper lexer, StringBuilder? builder);
 
 }
 
+/// <summary>
+/// Abstract base class for the BIS PreProcessor objects. Implements IBisPreProcessorBase.
+/// </summary>
 public abstract class BisPreProcessorBase : IBisPreProcessorBase
 {
-    protected BisPreProcessorBase()
-    {
-
-    }
-
     public abstract Result EvaluateLexer(IBisMutableStringStepper lexer, StringBuilder? builder);
 }
 
 
+/// <summary>
+/// Abstract base class for the generic BIS PreProcessor.
+/// <para>
+/// Provides the base functionality to be extended by specific preprocessors in the working context.
+/// </para>
+/// </summary>
+/// <typeparam name="TPreProcTypes">A set of types which the PreProcessor will work with, represented as an enumeration.</typeparam>
 public abstract class BisPreProcessor<TPreProcTypes> : BisPreProcessorBase, IBisLexer<TPreProcTypes> where TPreProcTypes : Enum
 {
+    /// <summary>
+    /// Event that fires when a token match occurs.
+    /// </summary>
     public event IBisLexer<TPreProcTypes>.TokenMatched? OnTokenMatched;
+
+    /// <summary>
+    /// Provides definitions for token types.
+    /// </summary>
     public abstract IEnumerable<IBisLexer<TPreProcTypes>.TokenDefinition> TokenTypes { get; }
+
     public abstract IBisLexer<TPreProcTypes>.TokenDefinition EOFToken { get; }
+
     public IBisLexer<TPreProcTypes>.TokenDefinition? ErrorToken => null;
+
     public IEnumerable<IBisLexer<TPreProcTypes>.TokenMatch> PreviousMatches => previousMatches;
+
     private readonly List<IBisLexer<TPreProcTypes>.TokenMatch> previousMatches = new();
+
     public IBisLexer<TPreProcTypes>.TokenMatch? PreviousMatch() => previousMatches.LastOrDefault();
+
+    /// <summary>
+    /// Produces the next available token from the provided lexer object. The method internally
+    /// calls the abstract GetNextToken() and then raises an OnTokenMatched event with the
+    /// retrieved token as an argument.
+    /// </summary>
+    /// <param name="lexer">The lexer (an instance of IBisMutableStringStepper)</param>
+    /// <returns>The next available token</returns>
     public IBisLexer<TPreProcTypes>.TokenMatch NextToken(IBisMutableStringStepper lexer)
     {
         var value = GetNextToken(lexer);
@@ -37,6 +71,13 @@ public abstract class BisPreProcessor<TPreProcTypes> : BisPreProcessorBase, IBis
         return value;
     }
 
+    /// <summary>
+    /// Gets the next available token given a mutable string stepper. This function should
+    /// only ever be accessed my NextToken to prevent token-swallowing.
+    /// DO NOT CALL THIS METHOD! HANDS OFF!!!
+    /// </summary>
+    /// <param name="lexer">Contextual mutable string stepper instance.</param>
+    /// <returns>The next available token.</returns>
     protected abstract IBisLexer<TPreProcTypes>.TokenMatch GetNextToken(IBisMutableStringStepper lexer);
 
     public void TokenizeUntilEnd<T>(BisLexer<T> lexer) where T : Enum
@@ -46,6 +87,17 @@ public abstract class BisPreProcessor<TPreProcTypes> : BisPreProcessorBase, IBis
         }
     }
 
+    /// <summary>
+    /// Tokenizes lexer input until a certain condition (expressed as a Func delegate) is met.
+    /// Populates a string builder with all tokens encountered until the condition becomes true.
+    /// The final string representation of accumulated tokens is used to create a new token match
+    /// which is returned.
+    /// </summary>
+    /// <typeparam name="TTypes">The enumeration that represents the set of types of tokens the lexer will work with.</typeparam>
+    /// <param name="lexer">The lexer object (an instance of BisLexer<![CDATA[<]]>TTypes<![CDATA[>]]>)</param>
+    /// <param name="until">A function that determines when the tokenization process should stop.</param>
+    /// <param name="asToken">The definition of the token to be generated from the accumulated text.</param>
+    /// <returns>A new token match that encapsulates the accumulated text.</returns>
     public IBisLexer<TPreProcTypes>.TokenMatch TokenizeUntil<TTypes>(
         BisLexer<TTypes> lexer,
         Func<IBisLexer<TPreProcTypes>.TokenMatch, bool> until, IBisLexer<TPreProcTypes>.TokenDefinition asToken) where TTypes : Enum
@@ -63,6 +115,10 @@ public abstract class BisPreProcessor<TPreProcTypes> : BisPreProcessorBase, IBis
 
     protected virtual void OnTokenMatchedHandler(IBisLexer<TPreProcTypes>.TokenMatch match, IBisLexer<TPreProcTypes> lexer) => OnTokenMatched?.Invoke(match, lexer);
 
+    /// <summary>
+    /// Adds a new match to the list of previous matches.
+    /// </summary>
+    /// <param name="match">Match instance to be added.</param>
     protected void AddPreviousMatch(IBisLexer<TPreProcTypes>.TokenMatch match) => previousMatches.Add(match);
 
 
--- src/File Formats/Languages/BisUtils.Param/Parse/ParamParser.cs
+++ src/File Formats/Languages/BisUtils.Param/Parse/ParamParser.cs
@@ -11,11 +11,33 @@ using Models.Literals;
 using Models.Statements;
 using PreProcessor.RV;
 
+/// <summary>
+/// The `ParamParser` class implements the `IBisParser` interface for parsing parameter files.
+/// It specifically parses `ParamFile` nodes using the `ParamLexer` and `ParamTypes` types.
+/// It also uses `RVPreProcessor` for preprocessing.
+/// </summary>
 public class ParamParser : IBisParser<ParamFile, ParamLexer, ParamTypes, RVPreProcessor>
 {
+    /// <summary>
+    /// Gets the current and only instance of the parser.
+    /// </summary>
     public static readonly ParamParser Instance = new();
 
+    private ParamParser()
+    {
+    }
+
 #pragma warning disable CA1822
+    /// <summary>
+    /// Parses a `ParamLexer` instance into a `ParamFile` node.
+    /// It supports parsing different parameter types like class
+    /// definitions, deletions, enums, and identifier assignment
+    /// with corresponding behaviors. This method throws a
+    /// `NotSupportedException` for unexpected tokens or syntax.
+    /// </summary>
+    /// <param name="node">The output `ParamFile` node.</param>
+    /// <param name="lexer">The `ParamLexer` instance to be parsed.</param>
+    /// <returns>A `Result` instance representing the result of the parsing operation.</returns>
     public Result Parse(out ParamFile? node, ParamLexer lexer)
     {
         var results = new List<Result>();
@@ -180,6 +202,14 @@ public class ParamParser : IBisParser<ParamFile, ParamLexer, ParamTypes, RVPrePr
 
     }
 
+    /// <summary>
+    /// Parses a literal into `IParamLiteralBase` instance which could be a `ParamFloat`,
+    /// `ParamInt` or `ParamString` depending on the literal token found.
+    /// This method throws a `NotSupportedException` for unexpected or invalid literal tokens.
+    /// </summary>
+    /// <param name="lexer">The `ParamLexer` providing the literal to parse.</param>
+    /// <param name="next">Output parameter to hand back the next token after parsing the literal.</param>
+    /// <returns>An `IParamLiteralBase` instance representing the parsed literal.</returns>
     private static IParamLiteralBase ParseLiteral(ParamLexer lexer, out IBisLexer<ParamTypes>.TokenMatch next)
     {
         var value = new ParamString((next = lexer.NextLiteral()).TokenText);
@@ -193,6 +223,11 @@ public class ParamParser : IBisParser<ParamFile, ParamLexer, ParamTypes, RVPrePr
             : value;
     }
 
+    /// <summary>
+    /// Determines the `ParamOperatorType` based on whether the parsed item is an AddAssign or SubAssign operator.
+    /// </summary>
+    /// <param name="isAddAssign">Boolean indicator to denote whether the operator is an AddAssign or not.</param>
+    /// <returns>A `ParamOperatorType` value representing the operator.</returns>
     private static ParamOperatorType OperatorOf(bool isAddAssign) => isAddAssign ? ParamOperatorType.AddAssign : ParamOperatorType.SubAssign;
 }
 
