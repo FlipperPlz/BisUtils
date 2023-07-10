0d248ac6e9898c8460cf0460c22076baaadc4476
ryann <flipperplz@protonmail.com>
2023-07-10 12:00:17 -0700

    feat(shape): Create Main Abstraction For Models

---
+++ src/File Formats/BisUtils.P3D/Models/RVShape.cs
@@ -0,0 +1,25 @@
+﻿namespace BisUtils.P3D.Models;
+
+using Core.Binarize;
+using Core.Binarize.Implementation;
+using Core.IO;
+using Options;
+
+public interface IRVShape<out TLodType> : IStrictBinaryObject<RVShapeOptions> where TLodType : IRVLod
+{
+    string ModelName { get; }
+    float ModelMass { get; }
+    IEnumerable<TLodType> LevelsOfDetail { get; }
+}
+
+public abstract class RVShape<TLodType> : StrictBinaryObject<RVShapeOptions>, IRVShape<TLodType> where TLodType : RVLod
+{
+    public string ModelName { get; }
+    public abstract float ModelMass { get; }
+    public abstract IEnumerable<TLodType> LevelsOfDetail { get; }
+
+    protected RVShape(string modelName) : base() => ModelName = modelName;
+
+    protected RVShape(string modelName, BisBinaryReader reader, RVShapeOptions options) : base(reader, options) => ModelName = modelName;
+
+}
---
+++ src/File Formats/BisUtils.P3D/Options/RVShapeOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace BisUtils.P3D.Options;
+
+using System.Text;
+using Core.Binarize.Options;
+using Core.Binarize.Utils;
+using Core.Options;
+
+public class RVShapeOptions : IBinarizationOptions, IAsciizLimiterOptions
+{
+    public Encoding Charset { get; set; } = Encoding.UTF8;
+    public Endianness ByteOrder { get; set; } = Endianness.Little;
+    public bool IgnoreValidation { get; set; }
+    public int AsciiLengthTimeout { get; set; }
+}
