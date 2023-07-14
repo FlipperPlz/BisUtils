9a768ebf33e02fefd6532ad0ec5784d906a3a8eb
ryann <flipperplz@protonmail.com>
2023-07-14 15:28:37 -0700

    feat: Add Missing ShapeTest Library
    Signed-off-by: ryann <flipperplz@protonmail.com>

---
+++ test/Scratches/ShapeTest/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BisUtils.Core.IO;
+using BisUtils.P3D.Models;
+using BisUtils.P3D.Options;
+
+var reader = new BisBinaryReader(File.OpenRead(@"C:\Users\ryann\Desktop\box.p3d"));
+var shapeOptions = new RVShapeOptions();
+var shape = new RVShape("box", reader, shapeOptions);
+Console.WriteLine("Done");
