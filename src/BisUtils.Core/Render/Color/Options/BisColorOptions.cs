2465940f4d524eaf2f5f8589f823d5a18efa3cc1
ryann <flipperplz@protonmail.com>
2023-07-17 07:25:57 -0700

    feat(texture): Create Color; Add RVTexture project;

---
+++ src/BisUtils.Core/Render/Color/Options/BisColorOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace BisUtils.Core.Render.Color.Options;
+
+using System.Text;
+using Binarize.Utils;
+
+public class BisColorOptions : IBisColorOptions
+{
+    public Encoding Charset { get; set; } = Encoding.UTF8;
+    public Endianness ByteOrder { get; set; }
+    public bool IgnoreValidation { get; set; }
+    public bool UsePackedColor { get; set; }
+}
---
+++ src/File Formats/BisUtils.RVTexture/Options/RVTextureOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace BisUtils.RVTexture.Options;
+
+using System.Text;
+using Core.Binarize.Options;
+using Core.Binarize.Utils;
+
+public class RVTextureOptions : IBinarizationOptions
+{
+    public Encoding Charset { get; set; } = Encoding.UTF8;
+    public Endianness ByteOrder { get; set; }
+    public bool IgnoreValidation { get; set; }
+}
