52d180a2ff58ff04b87bbc875bc37906b24388a9
ryann <flipperplz@protonmail.com>
2023-07-20 12:21:32 -0700

    feat(A2S): Add Library For A2S Server Queries

---
+++ src/BisUtils.DZServerQuery/Models/DzInfoQuery.cs
@@ -0,0 +1,39 @@
+﻿namespace BisUtils.DZServerQuery.Models;
+
+using System.Text;
+using Core.Binarize.Flagging;
+using Enumerations;
+
+public record DzInfoQuery(
+    byte ProtocolVersion,
+    string Name,
+    string Map,
+    string Folder,
+    string Game,
+    string Version,
+    short Identifier,
+    byte PlayerCount,
+    byte MaxPlayers,
+    byte BotCount,
+    SteamServerType SteamServerType,
+    DzSteamEnvironmentType SteamEnvironmentType,
+    SteamSeverVisibility SteamSeverVisibility,
+    bool VacSecured,
+    ulong? GameId,
+    ulong? SteamId,
+    string? Keywords,
+    string? Spectator,
+    short? SpectatorPort,
+    short? Port) : IBisFlaggable<DzInfoQueryFlags> {
+    private static readonly byte[] SendMagic, ReceiveMagic;
+    public DzInfoQueryFlags Flags { get; set; }
+
+    public static byte[] GetSendMagic() => SendMagic;
+    public static byte[] GetReceiveMagic() => ReceiveMagic;
+
+    static DzInfoQuery() {
+        SendMagic = new byte[] { (byte)DzSteamQueryCode.InfoCode }.Concat(Encoding.ASCII.GetBytes(IDzQuery.InfoQueryMessage)).Append(byte.MinValue).ToArray();
+        ReceiveMagic = new byte[] { (byte)DzSteamQueryCode.InfoResponse };
+    }
+
+}
---
+++ src/BisUtils.DZServerQuery/Models/DzServerMod.cs
@@ -0,0 +1,8 @@
+﻿namespace BisUtils.DZServerQuery.Models;
+
+public record DzServerMod(
+    string ModName,
+    long ModID,
+    int ModHash,
+    bool DLC
+);
