c1c6b51d14305359d65ae17a94a9fe380c5846db
ryann <flipperplz@protonmail.com>
2023-07-25 14:55:58 -0700

    chore(bank): Add BankTest Project
    Signed-off-by: ryann <flipperplz@protonmail.com>

---
+++ test/Scratches/BankTest/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using BisUtils.RVBank.Model;
+using BisUtils.RVBank.Options;
+using Microsoft.Extensions.Logging.Abstractions;
+
+var bankOptions = new RVBankOptions()
+{
+    AllowObfuscated = true,
+    FlatRead = false,
+    RemoveBenignProperties = true,
+    AllowMultipleVersion = true,
+    IgnoreValidation = true,
+    DecompressionTimeout = TimeSpan.FromMinutes(2),
+    Charset = Encoding.UTF8,
+    RequireValidSignature = false,
+    RegisterEmptyEntries = false,
+    CompressionErrorsAreWarnings = false,
+    RequireEmptyVersionMeta = false,
+    RequireVersionMimeOnVersion = false,
+    RequireFirstEntryIsVersion = false,
+    RequireVersionNotNamed = false,
+    AllowVersionMimeOnData = true,
+    IgnoreInvalidStreamSize = true,
+    AlwaysSeparateOnDummy = true,
+    AllowUnnamedDataEntries = true,
+    IgnoreDuplicateFiles = true,
+    RespectEntryOffsets = false,
+    WriteValidOffsets = false,
+    AsciiLengthTimeout = 510,
+    AllowEncrypted = true
+};
+
+var bank = RVBank.ReadPbo(@"C:\Users\ryann\Downloads\test.pbo", bankOptions, File.Open(@"C:\Users\ryann\Downloads\Ryann\out\out.pbo", FileMode.OpenOrCreate, FileAccess.ReadWrite), NullLogger.Instance);
+
+bank.SynchronizeWithStream(bankOptions);
+Console.WriteLine();
