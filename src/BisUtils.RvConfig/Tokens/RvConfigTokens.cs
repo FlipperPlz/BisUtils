9f38cf1d5bbda49f57cf13186e5c0502526399f1
ryann <flipperplz@protonmail.com>
2023-09-08 10:57:20 -0700

    feat(rvConfig): Add Support For Basic TokenTypes to ParamLexer
    Signed-off-by: ryann <flipperplz@protonmail.com>

--- src/BisUtils.RvConfig/Tokens/RvConfigTokens.cs
+++ src/BisUtils.RvConfig/Tokens/RvConfigTokens.cs
@@ -1,40 +1,82 @@
 ﻿namespace BisUtils.RvConfig.Tokens;
 
+using LangAssembler.Lexer.Base;
+using LangAssembler.Lexer.Models.Match;
+using LangAssembler.Lexer.Models.Type;
 using RvLex.Tokens;
 
 public interface IRvConfigTokenSet : IRvTokenSet
 {
+    public static abstract ITokenType RvRightCurly { get; }
+    TokenMatcher RightCurlyMatcher => MatchRightCurly;
+    static bool MatchRightCurly(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == '}';
 
+    public static abstract ITokenType RvLeftCurly { get; }
+    TokenMatcher LeftCurlyMatcher => MatchLeftCurly;
+    static bool MatchLeftCurly(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == '{';
+
+    public static abstract ITokenType RvRightSquare { get; }
+    TokenMatcher RightSquareMatcher => MatchRightSquare;
+    static bool MatchRightSquare(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == ']';
+
+    public static abstract ITokenType RvLeftSquare { get; }
+    TokenMatcher LeftSquareMatcher => MatchLeftSquare;
+    static bool MatchLeftSquare(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == '[';
+
+    public static abstract ITokenType RvColon { get; }
+    TokenMatcher ColonMatcher => MatchColon;
+    static bool MatchColon(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == ':';
+
+    public static abstract ITokenType RvSemicolon { get; }
+    TokenMatcher SemicolonMatcher => MatchSemicolon;
+    static bool MatchSemicolon(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == ';';
+
+
+    public static abstract ITokenType RvAssign { get; }
+    TokenMatcher AssignMatcher => MatchAssign;
+    static bool MatchAssign(ILexer lexer, long tokenStart, int? currentChar)
+        => currentChar == '=';
 }
+
 
 public sealed class RvConfigTokenSet : RvTokenSet, IRvConfigTokenSet
 {
-    public static readonly IRvConfigTokenSet Instance = new RvConfigTokenSet();
+    public static new readonly IRvConfigTokenSet Instance = new RvConfigTokenSet();
+    public static ITokenType RvRightCurly =>
+        new TokenType("param.symbol.curly.right", Instance.RightCurlyMatcher);
+    public static ITokenType RvLeftCurly =>
+        new TokenType("param.symbol.curly.left", Instance.LeftCurlyMatcher);
+    public static ITokenType RvRightSquare =>
+        new TokenType("param.symbol.square.left", Instance.LeftSquareMatcher);
+    public static ITokenType RvLeftSquare =>
+        new TokenType("param.symbol.square.right", Instance.RightSquareMatcher);
+    public static ITokenType RvColon =>
+        new TokenType("param.symbol.colon", Instance.ColonMatcher);
+    public static ITokenType RvSemicolon =>
+        new TokenType("param.symbol.semicolon", Instance.ColonMatcher);
+    public static ITokenType RvAssign =>
+        new TokenType("param.symbol.assign", Instance.AssignMatcher);
 
+    protected override void InitializeTypes()
+    {
+        InitializeType(RvRightCurly);
+        InitializeType(RvLeftCurly);
+        InitializeType(RvRightSquare);
+        InitializeType(RvLeftSquare);
+        InitializeType(RvColon);
+        InitializeType(RvSemicolon);
+        base.InitializeTypes();
+
+    }
     //
     // public static readonly IBisTokenType ConfigLiteral =
     //     new BisTokenType("param.abstract.literal", ToComplex);
-    //
-    // public static readonly IBisTokenType ConfigLCurly =
-    //     new BisTokenType("param.symbol.curly.left", "{");
-    //
-    // public static readonly IBisTokenType ConfigRCurly =
-    //     new BisTokenType("param.symbol.curly.right", "}");
-    //
-    // public static readonly IBisTokenType ConfigLSquare =
-    //     new BisTokenType("param.symbol.curly.left", "[");
-    //
-    // public static readonly IBisTokenType ConfigRSquare =
-    //     new BisTokenType("param.symbol.curly.right", "]");
-    //
-    // public static readonly IBisTokenType ConfigColon =
-    //     new BisTokenType("param.symbol.colon", ":");
-    //
-    // public static readonly IBisTokenType ConfigSeparator =
-    //     new BisTokenType("param.symbol.separator", ";");
-    //
-    // public static readonly IBisTokenType ConfigAssign =
-    //     new BisTokenType("param.symbol.assign", "=");
     //
     // public static readonly IBisTokenType ConfigSubAssign =
     //     new BisTokenType("param.symbol.assign.subtract", "-=");
